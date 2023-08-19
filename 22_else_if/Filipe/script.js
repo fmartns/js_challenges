7040906d8918929878f49556244b74ec8184cf2f
fmartns <hello@fmartns.dev>
2023-08-18 23:46:55 -0300

    js

---
+++ 22_else_if/Filipe/script.js
@@ -0,0 +1,15 @@
+let adm_user = "fmartns";
+let adm_pass = "1234";
+
+let user = prompt("user: ");
+let pass = prompt("password: ");
+
+if ((user == adm_user) && (pass == adm_pass)) {
+    alert("Login efetuado com sucesso!");
+} else if ((user == undefined) || (pass == undefined)) {
+    alert("Você deixou de digitar algum campo.");
+} else if ((user != adm_user) || (pass != adm_pass)) {
+    alert("Usuário inválido.");
+} else {
+    alert("Ocorreu um erro.");
+}
