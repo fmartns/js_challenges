797d29f6e1a0b19e5d2fb604d481749d654bbc6c
fmartns <hello@fmartns.dev>
2023-08-18 22:38:59 -0300

    js

---
+++ 18_math/script.js
@@ -0,0 +1,15 @@
+let maior = Math.max(6, 12, 28, 55, 8);
+
+console.log(maior);
+
+let menor = Math.min(6, 12, 28, 55, 8);
+
+console.log(menor);
+
+let arredondar = Math.round(2.93434435);
+
+console.log(arredondar);
+
+let arredondadorParaCima = Math.ceil(42.1323323);
+
+console.log(arredondadorParaCima);
---
+++ 21_else/scripts.js
@@ -0,0 +1,7 @@
+let velocidade = 134;
+
+if(velocidade <= 80) {
+    console.log('Sua velocidade é de: ' + velocidade + ' Kms/h.' + ' Boa viagem!')
+} else {
+    console.log('Sua velocidade é de: ' + velocidade + ' Kms/h.' + ' Reduza a velocidade!')
+}
