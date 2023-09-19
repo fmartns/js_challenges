5fb5739fad40ee4b01792aebce510ee84c2cbc37
fmartns <hello@fmartns.dev>
2023-09-19 09:04:29 -0300

    js

---
+++ 24_do_while/script.js
@@ -0,0 +1,6 @@
+let x = 10;
+
+do {
+    console.log("X é igual a " + x);
+    x = x - 1
+} while (x > 0);
---
+++ 25_for/script.js
@@ -0,0 +1,5 @@
+// Varíavel / Condição / Incremento
+
+for(let x = 0; x < 101; x = x + 5){
+    console.log("O valor de x é " + x);
+}
