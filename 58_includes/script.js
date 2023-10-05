91c826a546733ea30098e6db80743f2dbb806513
fmartns <hello@fmartns.dev>
2023-10-05 15:35:06 -0300

    js

---
+++ 58_includes/script.js
@@ -0,0 +1,4 @@
+let carros = ["BMW", "Fiat", "Audi"];
+
+console.log(carros.includes("Fiat"));
+console.log(carros.includes("Mercedes"))
---
+++ 59_reverse/script.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(arr.reverse());
+
+let carros = ["BWM", "Fiat", "VW", "Renault", "Audi"];
+
+console.log(carros.reverse());
